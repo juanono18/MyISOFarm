0291f53750bbb812b45d7646ec521284c0eecf94
Juan Álvarez Manzano <juantecno180@gmail.com>
2023-11-21 03:10:54 +0100

    Añadidas zanahoras y cambio de semilla básico

--- TileMap.gd
+++ TileMap.gd
@@ -2,13 +2,21 @@ extends TileMap
 
 
 # Called when the node enters the scene tree for the first time.
+var grownState={
+	Vector2i(8,0):true,
+	Vector2i(14,0):true
+}
+var seedState={
+	Vector2i(4,0):true,
+	Vector2i(10,0):true
+}
 var tilesID = {
 	"grass":Vector2i(0,0),
 	"dirt":Vector2i(1,0),
 	"hoed_dirt":Vector2i(2,0),
 	"watered_dirt":Vector2i(3,0),
-	"seeded_dirt":Vector2i(4,0),
-	"grown_crop":Vector2i(8,0)
+	"seeded_dirt":Vector2i(10,0),
+	"grown_crop":Vector2i(14,0)
 }
 var seededFields = []
 var dirtFields = []
@@ -32,19 +40,20 @@ func updateHoverOnTile():
 	if(get_cell_atlas_coords(1,tile) != Vector2i(-1,-1)):
 		if( get_cell_atlas_coords(1,tile) == tilesID["grass"] or  get_cell_atlas_coords(1,tile) == tilesID["dirt"]):
 			Input.set_custom_mouse_cursor(load("res://hoe_cursor.png"))
-		elif( get_cell_atlas_coords(0,Vector2(tile.x-1,tile.y-1)) == tilesID["seeded_dirt"]):
+		elif( get_cell_atlas_coords(0,Vector2i(tile.x-1,tile.y-1)) in seedState):
 			Input.set_custom_mouse_cursor(load("res://water_cursor.png"))
 		elif( get_cell_atlas_coords(1,tile) == tilesID["hoed_dirt"]):
 			Input.set_custom_mouse_cursor(load("res://seed_cursor.png"))
-		elif( get_cell_atlas_coords(0,Vector2(tile.x-1,tile.y-1)) == tilesID["grown_crop"]):
+		elif(get_cell_atlas_coords(0,Vector2i(tile.x-1,tile.y-1)) in grownState):
 			Input.set_custom_mouse_cursor(load("res://harvest_cursor.png"))
-		set_cell(2,Vector2(tile.x-1,tile.y-1),1,Vector2(9,0),0)
+		set_cell(2,Vector2(tile.x-1,tile.y-1),1,Vector2i(9,0),0)
 	if(prevTile != null):
 		#print("here")
 		if(tile != prevTile):
 			#print(prevTile)
 			set_cell(2,Vector2(prevTile.x-1,prevTile.y-1))
 	prevTile = tile
+	
 func setWslabel(label:Label):
 	wslabel = label
 	
@@ -55,6 +64,12 @@ func setSounds( dirtSound1: AudioStreamPlayer,harvestSound1: AudioStreamPlayer,p
 	waterSound=waterSound1
 	
 func _input(event):
+	if event.is_action_pressed("hotbar1"):
+		tilesID["seeded_dirt"]= Vector2i(4,0)
+		tilesID["grown_crop"]=Vector2i(8,0)
+	if event.is_action_pressed("hotbar2"):
+		tilesID["seeded_dirt"]= Vector2i(10,0)
+		tilesID["grown_crop"]=Vector2i(14,0)
 	if event.is_action_pressed("LeftClick"):
 		var tile = local_to_map(Vector2(get_global_mouse_position().x+5,get_global_mouse_position().y+10))
 		#print(get_cell_atlas_coords(1,tile),get_cell_atlas_coords(0,Vector2(tile.x-1,tile.y-1)))
@@ -62,13 +77,13 @@ func _input(event):
 		if(get_cell_atlas_coords(1,tile) == tilesID["grass"] or get_cell_atlas_coords(1,tile) == tilesID["dirt"]):
 			arar(tile)
 		#si es plantado regar
-		else: if(get_cell_atlas_coords(0,Vector2(tile.x-1,tile.y-1)) == Vector2i(4,0)):
+		elif(get_cell_atlas_coords(0,Vector2(tile.x-1,tile.y-1)) in seedState):
 			regar(tile)
 		#si es cultivo recoger y poner tierra
-		else: if(get_cell_atlas_coords(0,Vector2(tile.x-1,tile.y-1)) ==tilesID["grown_crop"]):
+		elif(get_cell_atlas_coords(0,Vector2i(tile.x-1,tile.y-1)) in grownState):
 			cosechar(tile)
 		#Si es arado plantar
-		else: if(get_cell_atlas_coords(1,tile) == tilesID["hoed_dirt"]):
+		elif(get_cell_atlas_coords(1,tile) == tilesID["hoed_dirt"]):
 			sembrar(tile)
 		
 
@@ -90,24 +105,25 @@ func cosechar(tile):
 	wslabel.text = str("%07d" % WheatCount)
 func sembrar(tile):
 	plantSound.play()
-	set_cell(0,Vector2(tile.x-1,tile.y-1),1,Vector2(4,0),0)
+	set_cell(0,Vector2(tile.x-1,tile.y-1),1,tilesID["seeded_dirt"],0)
 
 func _on_wheat_timer_timeout():
 	
 	for seed in seededFields:
 		#print("growth")
+		var tile = get_cell_atlas_coords(0,seed["TileID"])
 		match seed["GrowthState"]:
 			0:
-				set_cell(0,seed["TileID"],1,Vector2(5,0),0)
+				set_cell(0,seed["TileID"],1,Vector2(tile.x+1,tile.y),0)
 				seed["GrowthState"] = seed["GrowthState"]+1
 			1:
-				set_cell(0,seed["TileID"],1,Vector2(6,0),0)
+				set_cell(0,seed["TileID"],1,Vector2(tile.x+1,tile.y),0)
 				seed["GrowthState"] = seed["GrowthState"]+1
 			2:
-				set_cell(0,seed["TileID"],1,Vector2(7,0),0)
+				set_cell(0,seed["TileID"],1,Vector2(tile.x+1,tile.y),0)
 				seed["GrowthState"] = seed["GrowthState"]+1
 			3:
-				set_cell(0,seed["TileID"],1,Vector2(8,0),0)
+				set_cell(0,seed["TileID"],1,Vector2(tile.x+1,tile.y),0)
 				seededFields.erase(seed)
 
 
